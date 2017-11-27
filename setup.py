5b3f0e5c60bee07b706b8038993362d6766149a8
dante <iamdanteonline@gmail.com>
2017-11-27 13:15:24 +0300

    add project to pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-dantejcoder',
+    version='0.1',
+    packages=['dantejcoder'],
+    include_package_data=True,
+    license='GNU General Public License v3.0', 
+    description='A simple Django app to covert standard django-models to json',
+    long_description=README,
+    url='https://github.com/DanteOnline/django-dantejcoder',
+    author='DanteOnline',
+    author_email='iamdanteonline@gmail.com',
+    keywords = ['json', 'django', 'JsonResponse', 'encoder'],
+    classifiers = []
+)
